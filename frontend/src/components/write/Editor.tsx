070de9c55ed14615d5549f267c0d6a23fe40710e
Moomi98 <kymkjh2002@gmail.com>
2023-11-26 17:44:02 +0900

    [frontend] Editor에 imageHandler 적용

--- frontend/src/components/write/Editor.tsx
+++ frontend/src/components/write/Editor.tsx
@@ -1,8 +1,13 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import ReactQuill from "react-quill";
+import { RangeStatic } from "quill";
 import "react-quill/dist/quill.snow.css";
+import { storage } from "@/src/firebase/firebase";
+import { uploadBytes, getDownloadURL, ref } from "firebase/storage";
+import { useRecoilValue } from "recoil";
+import { UserState } from "@/src/stores/User";
 
 interface EditorProps {
   onUpdateValue: Function;
@@ -10,20 +15,12 @@ interface EditorProps {
 
 export default function Editor(props: EditorProps) {
   const [value, setValue] = useState("");
+  const quillRef = useRef<ReactQuill>(null);
+  const user = useRecoilValue(UserState);
 
   useEffect(() => {
     props.onUpdateValue(value);
   }, [value, props]);
-
-  const toolbarOptions = [
-    ["link", "image"],
-    [{ header: [1, 2, 3, false] }],
-    ["bold", "italic", "underline", "strike"],
-    ["blockquote"],
-    [{ list: "ordered" }, { list: "bullet" }],
-    [{ color: [] }, { background: [] }],
-    [{ align: [] }],
-  ];
 
   const formats = [
     "header",
@@ -45,15 +42,62 @@ export default function Editor(props: EditorProps) {
     "width",
   ];
 
-  const modules = {
-    toolbar: {
-      container: toolbarOptions,
-    },
-  };
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          ["link", "image"],
+          [{ header: [1, 2, 3, false] }],
+          ["bold", "italic", "underline", "strike"],
+          ["blockquote"],
+          [{ list: "ordered" }, { list: "bullet" }],
+          [{ color: [] }, { background: [] }],
+          [{ align: [] }],
+        ],
+        handlers: {
+          image: () => {
+            const input = document.createElement("input");
+            input.setAttribute("type", "file");
+            input.setAttribute("accept", "image/*");
+            input.click();
+            input.addEventListener("change", async () => {
+              if (!quillRef.current || !input.files) return;
+
+              const editor = quillRef.current.getEditor();
+              const file = input.files[0];
+              const range = editor.getSelection(true);
+              try {
+                // 파일명을 "image/Date.now()"로 저장
+                const storageRef = ref(
+                  storage,
+                  `image/${user.id}-${user.nickname}/${Date.now()}`
+                );
+                // Firebase Method : uploadBytes, getDownloadURL
+                await uploadBytes(storageRef, file).then((snapshot) => {
+                  getDownloadURL(snapshot.ref).then((url) => {
+                    // 이미지 URL 에디터에 삽입
+                    editor.insertEmbed(range.index, "image", url);
+                    // URL 삽입 후 커서를 이미지 뒷 칸으로 이동
+                    editor.setSelection(
+                      (range.index + 1) as unknown as RangeStatic
+                    );
+                  });
+                });
+              } catch (error) {
+                console.log(error);
+              }
+            });
+          },
+        },
+      },
+    }),
+    [user]
+  );
   return (
     <ReactQuill
       className="flex flex-col h-3/4"
       theme="snow"
+      ref={quillRef}
       value={value}
       onChange={setValue}
       modules={modules}
